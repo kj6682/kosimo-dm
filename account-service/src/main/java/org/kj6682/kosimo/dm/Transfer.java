e5ddb9df9a33d3122174d1709c9466c55d1ee93d
Luigi <luigi@sgt.pepper>
2016-04-23 19:00:16 +0200

    add Account and Transfer behaviour

---
+++ account-service/src/main/java/org/kj6682/kosimo/dm/Transfer.java
@@ -0,0 +1,7 @@
+package org.kj6682.kosimo.dm;
+
+/**
+ * Created by luigi on 23.04.16.
+ */
+public class Transfer {
+}
--- account-service/src/test/java/org/kj6682/kosimo/dm/AccountServiceApplicationTests.java
+++ account-service/src/test/java/org/kj6682/kosimo/dm/AccountServiceApplicationTests.java
@@ -1,16 +1,64 @@
 package org.kj6682.kosimo.dm;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.math.BigDecimal;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = AccountServiceApplication.class)
 public class AccountServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    Account debit;
+    Account credit;
+
+    @Autowired
+    AccountRepository accountRepo;
+
+    @Autowired
+    Transfer transfer;
+
+    @Before
+    public void init() {
+        debit = new Account();
+        credit = new Account();
+
+        debit.setBalance(BigDecimal.valueOf(100L));
+        credit.setBalance(BigDecimal.ZERO);
+
+        accountRepo.save(debit);
+        accountRepo.save(credit);
+    }
+
+
+    @Test
+    public void contextLoads() {
+
+        assertNotNull(debit);
+        assertNotNull(credit);
+        assertEquals(100L, debit.getBalance().longValue());
+        assertEquals(0L, credit.getBalance().longValue());
+
+        HttpEntity result = transfer.execute(String.valueOf(debit.getId()),
+                String.valueOf(credit.getId()),
+                BigDecimal.valueOf(100L),
+                "EUR");
+        assertEquals("Amount for account 5 is now 0", result.getBody());
+        assertEquals("Amount for account 5 is now 0", result.getBody());
+
+        debit = accountRepo.findOne(debit.getId());
+        credit = accountRepo.findOne(credit.getId());
+        assertEquals(0L, debit.getBalance().longValue());
+        assertEquals(100L, credit.getBalance().longValue());
+
+    }
 
 }
