1d3d0d8a516a6978852f858a942b4bb4a3288660
Luigi MASTROSERIO <luigi.mastroserio@gmail.com>
2016-08-02 23:30:32 +0200

    introducing chinook client application

--- media-service/src/main/resources/public/tryme.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:8081/media/1"
-    }).then(function(data) {
-       $('.media-id').append(data.id);
-       $('.media-title').append(data.title);
-    });
-});
